8007048f819d2c8ad6a105ad5725709eaf420ee7
Vitalis <vitalismaina18@gmail.com>
2023-11-22 17:48:50 +0300

    install axios and include amount field

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,69 @@
-'use client'
-
+'use client';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function StkPush() {
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [amount, setAmount] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const amountNumber = Number(amount);
 
-    const response = await fetch('/api/stkpush', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ phoneNumber }),
-    });
+    try {
+      setLoading(true);
 
-    if (response.ok) {
-      console.log('STK push initiated successfully');
-    } else {
-      console.error('Failed to initiate STK push');
+      const response = await axios.post('/api/v1/stkPush/', {
+        phoneNumber,
+        amount: amountNumber,
+      });
+
+      if (response.status === 200) {
+        alert('STK push initiated successfully');
+      } else {
+        alert('Failed to initiate STK push');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while initiating STK push');
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',backgroundColor:'goldenrod' }}>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', padding: '20px', border: '1px solid #ccc', borderRadius: '10px' }}>
-      Enter Phone Number
-
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'goldenrod' }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', padding: '20px', border: '1px solid #ccc', borderRadius: '10px' }}
+      >
+         Enter Phone Number
         <label>
+         
           <input
             type="text"
             value={phoneNumber}
             required
             onChange={(e) => setPhoneNumber(e.target.value)}
-            style={{ margin: '10px 0', padding: '10px', borderRadius: '5px', border: '1px solid #ccc',backgroundColor: '#555555' }}
+            style={{ margin: '10px 0', padding: '10px', borderRadius: '5px', border: '1px solid #ccc', backgroundColor: '#555555', color: 'white' }}
           />
         </label>
-        <button type="submit" style={{ padding: '10px 20px', borderRadius: '5px', border: 'none', color: 'white', cursor: 'pointer' }}>Initiate STK Push</button>
+        Enter Amount
+        <label>
+         
+          <input
+            type="text"
+            value={amount}
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            style={{ margin: '10px 0', padding: '10px', borderRadius: '5px', border: '1px solid #ccc', backgroundColor: '#555555', color: 'white' }}
+          />
+        </label>
+
+        <button type="submit" style={{ padding: '10px 20px', borderRadius: '5px', border: 'none', color: 'white', cursor: 'pointer', backgroundColor: loading ? '#999' : 'green' }} disabled={loading}>
+          {loading ? 'Initiating...' : 'Initiate STK Push'}
+        </button>
       </form>
     </div>
   );
