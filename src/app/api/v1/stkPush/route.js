e2dadc8c4469a451345cee4e08148dc9202ec327
Vitalis <vitalismaina18@gmail.com>
2023-11-22 17:50:59 +0300

    add a stkpush api route

---
+++ src/app/api/v1/stkPush/route.js
@@ -0,0 +1,89 @@
+'use server'
+import { NextResponse } from 'next/server';
+
+
+import axios from 'axios';
+
+export async function POST(req) {
+  const { phoneNumber, amount } =  await req.json();
+  console.log(phoneNumber,amount)
+  console.log(typeof(phoneNumber))
+  console.log(typeof(amount))
+
+    // Validate the phoneNumber and amount
+    if (!phoneNumber || !amount) {
+        return NextResponse.json({ error: 'Missing phoneNumber or amount.' })
+      }
+      if (typeof phoneNumber !== 'string' || !/^(0|\+?254|254)\d{9}$/.test(phoneNumber)) {
+        return NextResponse.json({ error: 'Invalid phoneNumber.' })
+      }
+      if (typeof amount !== 'number' || amount <= 0) {
+        return NextResponse.json({ error: 'Invalid amount.' })
+      }
+      console.log('number provided:', phoneNumber)
+      const formattedPhoneNumber = phoneNumber.replace(/^(0|\+?254)/, '254')
+
+      console.log('after formatting', formattedPhoneNumber)
+
+
+  const consumerKey = process.env.MPESA_CONSUMER_KEY;
+  const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+  const shortcode = process.env.MPESA_SHORTCODE;
+
+  // Generate an access token
+  const response = await axios.get(process.env.MPESA_OAuth2Token_URL, {
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64')}`,
+    },
+  });
+
+  const { access_token } = response.data;
+
+  // Get the current date and time
+  const now = new Date();
+
+  // Extract date and time components
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // months are 0-indexed in JavaScript
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  // Combine components into a timestamp string
+  const timestamp = `${year}${month}${day}${hours}${minutes}${seconds}`;
+
+  // Generate the password by concatenating shortcode, passkey and timestamp, then encoding it in base64
+  const password = Buffer.from(`${shortcode}${process.env.MPESA_PASSKEY}${timestamp}`).toString('base64');
+
+  // Construct the STK Push request payload
+  const payload = {
+    BusinessShortCode: shortcode,
+    Password: password,
+    Timestamp: timestamp,
+    TransactionType: 'CustomerPayBillOnline',
+    Amount: amount,
+    PartyA: formattedPhoneNumber,
+    PartyB: shortcode,
+    PhoneNumber: formattedPhoneNumber,
+    CallBackURL: "https://intimate-barnacle-reasonably.ngrok-free.app",
+    AccountReference: process.env.MPESA_INITIATOR_NAME,
+    TransactionDesc: 'testing ',
+  };
+
+  // Initiate the STK Push payment
+  const { data: stkPushResponse } = await axios.post(process.env.MPESA_STKPush_URL, payload, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      'Content-Type': 'application/json'
+    }
+  })
+
+  const stkPushData = stkPushResponse.data;
+
+  console.log('STK Push initiated successfully', stkPushData);
+
+  
+    return NextResponse.json({ message: 'STK push initiated successfully' });
+  } 
+  
